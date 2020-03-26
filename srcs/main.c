7a4765da18df16a2b6a24d40dd28538aef2988a9
ezalos <ezalos@gmail.com>
2020-03-26 18:47:14 +0100

    Begining 2

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,39 @@
 /*   By: ezalos <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/26 17:51:29 by ezalos            #+#    #+#             */
-/*   Updated: 2020/03/26 17:54:03 by ezalos           ###   ########.fr       */
+/*   Updated: 2020/03/26 18:46:59 by ezalos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "includes/head.h"
 
-
-
-
-
-int		main(void)
+t_vector	*fesitel(t_crypt *to_crypt)
 {
-	
-			
-			
-			
+
+}
+
+void		rot(t_vector *vct, int rot)
+{
+	int		i;
+
+	i = -1;
+	while (++i < vct->len)
+		vct->str[i] = (vct->str[i] + rot) % 256;
+}
+
+int		init(t_crypt *crypto, char *msg, char *key)
+{
+	crypto->msg = vectorise(msg)
+	crypto->key = vectorise(key)
+}
+
+int		main(int ac, char **av)
+{
+	t_crypt	crypto;
+
+	if (ac > 2)
+	{
+		init(&crypto, av[1], av[2]);
+	}
 	return (0);
 }
