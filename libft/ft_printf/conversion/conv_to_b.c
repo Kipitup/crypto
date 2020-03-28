dde134805deb1765fe881f371ce982d833c1081a
amartino <a.martinod1@gmail.com>
2020-03-28 19:03:56 +0100

    change printf to padd %b

--- libft/ft_printf/conversion/conv_to_b.c
+++ libft/ft_printf/conversion/conv_to_b.c
@@ -6,11 +6,26 @@
 /*   By: fkante <fkante@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/20 09:26:27 by fkante            #+#    #+#             */
-/*   Updated: 2020/01/20 11:42:05 by amartino         ###   ########.fr       */
+/*   Updated: 2020/03/28 18:58:21 by amartinod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
+
+static void		add_padding_for_base_2(t_vector *nb)
+{
+	size_t	i;
+
+	i = 8;
+	while (i < nb->len)
+	{
+		vct_addchar_at(nb, ' ', i);
+		i += 8;
+	}
+	if (nb->len % 8 != 0)
+		vct_fill_before(nb, '0', (8 - (nb->len % 8)));
+}
 
 int8_t			conv_b(t_vector *vector, t_flag *flag, t_vector *nb_itoa)
 {
@@ -36,7 +51,6 @@ t_vector		*conv_to_b(va_list *arg_pf, t_flag *flag)
 	nbr = va_arg(*arg_pf, uint64_t);
 	vector = vct_new(flag->width);
 	nb_itoa = vct_new(0);
-	str_itoa = NULL;
 	cancel_flag_for_numeric_conv(flag);
 	if ((str_itoa = ft_ox_itoa_base(nbr, base, flag->option)) == NULL)
 		vct_del(&nb_itoa);
@@ -44,6 +58,8 @@ t_vector		*conv_to_b(va_list *arg_pf, t_flag *flag)
 		if ((vct_addstr(nb_itoa, str_itoa)) == FAILURE)
 			vct_del(&nb_itoa);
 	ft_strdel(&str_itoa);
+	if (base == 2 && nb_itoa != NULL)
+		add_padding_for_base_2(nb_itoa);
 	if ((conv_b(vector, flag, nb_itoa)) == FAILURE)
 		vct_del(&vector);
 	vct_del(&nb_itoa);
