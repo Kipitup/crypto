efc05ba907a8dac24af8f8f1bd45a59ae0ad21fa
ezalos <ezalos@gmail.com>
2020-04-05 13:36:37 +0200

    Python test script working and with nice output

--- test/ultimate_testor.py
+++ test/ultimate_testor.py
@@ -2,22 +2,23 @@ from os import listdir
 from os.path import isfile, join
 from os import walk
 import os
+import time
 import random
-
+from color import *
 
 def execute(msg, key, dest, cycles, crypt):
     if crypt:
         command_line = "./crypto " + msg + " " + key + " " + str(cycles) + " CRYPT" + " 1>" + dest
     else:
         command_line = "./crypto " + msg + " " + key + " " + str(cycles) + " UNCRYPT" + " 1>" + dest
-    print("COMMAND: ", command_line)
+    #print("COMMAND: ", command_line)
     answer = os.system(command_line)
     return answer
 
 def diff(file_1, file_2, on):
-    command_line = "diff -a " + file_1 + " " + file_2
-    print("COMMAND: ", command_line)
-    print("DIIF IS:")
+    command_line = "diff -ay --color " + file_1 + " " + file_2
+    #print("COMMAND: ", command_line)
+    #print("DIIF IS:")
     if not on:
         answer = os.system(command_line)
     else:
@@ -36,12 +37,6 @@ def write_file(path, content):
     file.write(content)
     file.close()
 
-def get_list_of_files(path):
-    list_of_files = []
-    for (dirpath, dirnames, filenames) in walk(path):
-        for file in filenames:
-            list_of_files.append(file)
-    return list_of_files
 
 
 directory = "./test/"
@@ -50,30 +45,90 @@ path_to_crypt = directory + "crypted/"
 path_to_uncrypt = directory + "uncrypted/"
 
 
-def test_me(file_1, file_2):
-    cycles = 50
-    file_3 = file_1 + "_" + file_2
-    #Crypt
-    res = execute(path_to_tests + file_1, path_to_tests + file_2, path_to_crypt + file_3, cycles, 1)
-    diff(path_to_tests + file_1, path_to_crypt + file_3, 1)
-    
-    #Uncrypt
-    res = execute(path_to_crypt + file_3, path_to_tests + file_2, path_to_uncrypt + file_3, cycles, 0)
-    diff(path_to_tests + file_1, path_to_uncrypt + file_3, 0)
 
+class Testor():
+    def __init__(self):
+        self.max_len = -1
+        self.get_list_of_files()
+
+    def new_test(self):
+        self.select_msg_and_key()
+        self.cycles = random.randint(0, 500)
+        self.file_3 = self.file_1 + "_" + self.file_2
+        self.msg = path_to_tests + self.file_1
+        self.key = path_to_tests + self.file_2
+        self.cypher = path_to_crypt + self.file_3
+        self.uncrypted = path_to_uncrypt + self.file_3
+
+    def select_msg_and_key(self):
+        self.file_1 = self.list_of_files[random.randint(0, len(self.list_of_files) - 1)]
+        self.file_2 = self.list_of_files[random.randint(0, len(self.list_of_files) - 1)]
+
+    def get_list_of_files(self):
+        self.list_of_files = []
+        for (dirpath, dirnames, filenames) in walk(path_to_tests):
+            for file in filenames:
+                if self.max_len < len(file):
+                    self.max_len = len(file)
+                self.list_of_files.append(file)
+        self.max_len = (self.max_len * 2) + 1 + len(path_to_uncrypt)
+
+    def test_me(self):
+        #Crypt
+        self.display(1)
+        start = time.time()
+        res = execute(self.msg, self.key, self.cypher, self.cycles, 1)
+        time_took = time.time() - start
+        diff(self.msg, self.cypher, 0)
+        print(PURPLE, "\n Time to crypt    ", str(time_took)[0:5])
+        print(          " Time for a cycle ", str((time_took) / self.cycles)[0:7], RESET)
+        print("Need more details ? Feel free to copy next line")
+        print("cat -e ", self.msg, " && ", "cat -e ", self.cypher)
+        print("\n\n\n")
+    
+        #Uncrypt
+        self.display(0)
+        start = time.time()
+        res = execute(self.cypher, self.key, self.uncrypted, self.cycles, 0)
+        time_took = time.time() - start
+        diff(self.msg, self.uncrypted, 0)
+        print(PURPLE, "\n Time to crypt    ", str(time_took)[0:5])
+        print(          " Time for a cycle ", str((time_took) / self.cycles)[0:7], RESET)
+        print("Need more details ? Feel free to copy next line")
+        print("cat -e ", self.msg, " && ", "cat -e ", self.uncrypted)
+        print("\n\n\n")
+
+
+    def display(self, crypt = 1):
+        if crypt:
+            print(PURPLE, "=========CRYPT=========")
+            print(RED,    "Message ", self.msg)
+            print(YELLOW, "Key     ", self.key)
+            print(BLUE,   "Cycles    ", self.cycles)
+            print(GREEN,  "to file ", self.cypher, RESET)
+        else:
+            print(PURPLE, "========UNCRYPT========")
+            print(RED,    "Message ", self.cypher)
+            print(YELLOW, "Key     ", self.key)
+            print(BLUE,   "Cycles    ", self.cycles)
+            print(GREEN,  "to file ", self.uncrypted, RESET)
+
+def reset_folders():
+    os.system("rm -rf " + path_to_crypt)
+    os.system("mkdir -p " + path_to_crypt)
+    print("Cypher    folder has been reset !")
+    os.system("rm -rf " + path_to_uncrypt)
+    os.system("mkdir -p " + path_to_uncrypt)
+    print("Uncrypted folder has been reset !")
+    os.system("make")
 
 
 if __name__ == "__main__":
-    os.system("make")
-    os.system("rm -rf " + directory + path_to_crypt)
-    os.system("mkdir -p " + directory + path_to_crypt)
-    os.system("rm -rf " + directory + path_to_uncrypt)
-    os.system("mkdir -p " + directory + path_to_uncrypt)
-    list_of_tests = get_list_of_files(path_to_tests)
+    reset_folders()
+    testor = Testor()
     for i in range(1):
-        uno = random.randint(0, len(list_of_tests) - 1)
-        dos = random.randint(0, len(list_of_tests) - 1)
-        test_me(list_of_tests[uno], list_of_tests[dos])
+        testor.new_test()
+        testor.test_me()
 
 
 #get directory
